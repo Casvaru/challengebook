f3eb5537708907e03f47494c0435347ab15a5090
carlos segura <nocarlos.x@gmail.com>
2023-03-01 18:27:49 -0500

    Rebuild with React-Vite-Test

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className="App">
+      <header className='p-5'>
+        <h1 className='text-2xl font-bold'>Challenge book</h1>
+      </header>
+      <section className='px-7'>
+        <h2 className='font-semibold'>Febrero | UI Tailwindcss</h2>
+        <ol>
+          <li className='px-3'><a className='hover:underline' href="">↪027-MusicPlayer</a></li>
+        </ol>
+      </section>
+    </div>
+  )
+}
+
+export default App
